3dd4622488adc33fbaefe3f7a9fac09bdecc8886
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2025-01-05 23:01:24 -0800

    Finished ListPractice

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,27 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
-
+    List<String> aList = new ArrayList<>();
     // Add 3 elements to the list (OK to do one-by-one)
-
+    aList.add("apple");
+    aList.add("orange");
+    aList.add("banana");
     // Print the element at index 1
-
+    System.out.println(aList.get(1));
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
-
+    aList.set(1, "cumquat");
     // Insert a new element at index 0 (the length of the list will change)
-
+    aList.add(0, "grapes");
     // Check whether the list contains a certain string
-
+    if (aList.contains("avocado")) {
+      System.out.println("The list contains 'avocado'.");
+    } else {
+      System.out.println("The list does not contain 'avocado'.");
+    }
     // Iterate over the list using a traditional for-loop.
     // Print each index and value on a separate line
-
+    for (int i = 0; i < aList.size(); i++) {
+      System.out.println("Index: " + i + ", Value: " + aList.get(i));
+    }
     // Sort the list using the Collections library
-
+    Collections.sort(aList);
     // Iterate over the list using a for-each loop
     // Print each value on a second line
+    for (String element : aList) {
+      System.out.println(element);
+    }
 
     /*
      * Usage tip!
