238e80c62c1c14816246ada4af00105995465d01
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2025-01-06 16:40:15 -0800

    finished MapPractice

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -1,29 +1,42 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapPractice {
   public static void main(String[] args) {
     // Create a HashMap with String keys and Integer values and
     // assign it to a variable of type Map
-
+    Map<String, Integer> mappy = new HashMap<>();
     // Put 3 different key/value pairs in the Map
     // (it's OK to do this one-by-one)
-
+    mappy.put("kitties", 3);
+    mappy.put("doggies", 4);
+    mappy.put("fishies", 11);
     // Get the value associated with a given key in the Map
-
+    System.out.println(mappy.get("fishies"));
     // Find the size (number of key/value pairs) of the Map
-
+    System.out.println(mappy.size());
     // Replace the value associated with a given key (the size of the Map shoukld not change)
-
+    mappy.replace("kitties", 5);
     // Check whether the Map contains a given key
-
+    boolean qKey = mappy.containsKey("doggies");
+    System.out.println(qKey);
     // Check whether the Map contains a given value
-
+    boolean qValue = mappy.containsValue("2");
+    System.out.println(qValue);
     // Iterate over the keys of the Map, printing each key
-
+    for (String elementKey : mappy.keySet()) {
+      System.out.println(elementKey);
+    }
     // Iterate over the values of the map, printing each value
-
+    for (Integer elementValue : mappy.values()) {
+      System.out.println(elementValue);
+    }
     // Iterate over the entries in the map, printing each key and value
-
+    // Map.Entry<String, Integer> entry I had to ask for help from chatGPT on this part
+    for (Map.Entry<String, Integer> entry : mappy.entrySet()) {
+      System.out.println("Key: " + entry.getKey() + ", Value: " +
+      entry.getValue());
+    }
     /*
      * Usage tip!
      * 
